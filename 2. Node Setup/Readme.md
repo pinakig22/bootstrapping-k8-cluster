1c3dc1515b1006617b903689757fad03f52de017
Pinaki Ghosh <pinakighosh.rhce@gmail.com>
2023-10-25 11:08:26 +0530

    updated readme and master script

--- 2. Node Setup/Readme.md
+++ 2. Node Setup/Readme.md
@@ -28,6 +28,9 @@ To install on the `APT` based operating systems, set the environment variable **
 
 ## Run the following as root ##
 ## Do this on all Control plane nodes ##
+
+### switch to root user and update ###
+sudo -i
 apt update -y
 
 ##################
@@ -52,7 +55,7 @@ lsmod | grep br_netfilter
 lsmod | grep overlay
 
 ## Set up required sysctl params, these persist across reboots. ##
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
--- scripts/master-userdata.sh
+++ scripts/master-userdata.sh
@@ -21,3 +21,65 @@ else
   echo -e "This is node3\n"
   sudo hostnamectl set-hostname master-1c
 fi
+
+# disable swap
+sudo swapoff -a
+
+# keeps the swaf off during reboot
+(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
+sudo apt-get update -y
+
+## Create the .conf file to load the modules at startup ##
+cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+## Set up required sysctl params, these persist across reboots. ##
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+## Apply sysctl params without reboot ##
+sudo sysctl --system
+
+######################################
+# Install packaged versions of CRI-O #
+######################################
+## This is Operating system version as per above instruction ##
+export OS="xUbuntu_22.04"
+
+## This is kubernetes version for which CRI-O will be installed ##
+export VERSION="1.27"
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+deb [signed-by=/usr/share/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
+EOF
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+deb [signed-by=/usr/share/keyrings/libcontainers-crio-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
+EOF
+
+sudo mkdir -p /usr/share/keyrings
+
+sudo curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | gpg --dearmor -o /usr/share/keyrings/libcontainers-archive-keyring.gpg
+sudo curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/Release.key | gpg --dearmor -o /usr/share/keyrings/libcontainers-crio-archive-keyring.gpg
+
+sudo apt-get update
+sudo apt-get install cri-o cri-o-runc -y
+
+## Reload systemd manager configuration ##
+sudo systemctl daemon-reload
+
+## Enable to start at boot ##
+sudo systemctl enable crio --now
+
+## Start crio service ##
+systemctl start crio.service
+
+echo "CRI runtime installed successfully"
